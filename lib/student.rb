cb8f27ea0d1377100ee7b4e9c67e9fd415706005
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-30 23:54:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,7 +45,7 @@ def self.create(name, grade)
 end
 
 def self.new_from_db(row)
-  student= self.new(name,grade)
+  student= self.new(id=nil,name=nil,grade=nil)
   student.id = row[0]
   student.name = row[1]
   student.grade = row[2]
